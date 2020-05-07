93da812005f51e5f4be129bd03f4a1b3d430fd0e
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-08 02:21:59 +0300

    retrofit singleton

---
+++ MovieCentral/app/src/main/java/com/example/moviecentral/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.example.moviecentral.utils;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://api.themoviedb.org";
+    public static final String LANGUAGE = "TR";
+
+}
