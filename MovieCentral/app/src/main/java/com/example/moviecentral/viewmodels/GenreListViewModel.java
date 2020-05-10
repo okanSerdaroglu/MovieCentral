b404f88eabdb94be8c77a5f05ab1e1fa8f527529
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-10 23:55:32 +0300

    layouts added for recyclerView and it's itemView

---
+++ MovieCentral/app/src/main/java/com/example/moviecentral/viewmodels/GenreListViewModel.java
@@ -0,0 +1,13 @@
+package com.example.moviecentral.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class GenreListViewModel extends AndroidViewModel {
+
+    public GenreListViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
