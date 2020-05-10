e2bcb28ee26d0921ea1efb04ff84c73a2de5f403
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-05-11 02:32:39 +0300

    ApiResponse added and response type changed MovieAPI

---
+++ MovieCentral/app/src/main/java/com/example/moviecentral/responses/ApiResponse.java
@@ -0,0 +1,66 @@
+package com.example.moviecentral.responses;
+
+import java.io.IOException;
+
+import retrofit2.Response;
+
+public class ApiResponse<T> {
+
+    public ApiResponse<T> create(Throwable error){
+        return new ApiErrorResponse<>
+                (!error.getMessage().equals("") ?  error.getMessage()
+                        : "Unknown error \n check network connection");
+    }
+
+    public ApiResponse<T> create (Response<T> response){
+        if (response.isSuccessful()){
+            T body = response.body();
+
+            if (body == null || response.code() == 204){ // 204 means empty response code
+                return new ApiEmptyResponse<>();
+            }else {
+                return new ApiSuccessResponse<>(body);
+            }
+        } else {
+            String errorMsg = "";
+            try {
+                errorMsg = response.errorBody().string();
+            }catch (IOException e){
+                e.printStackTrace();
+                errorMsg = response.message();
+            }
+            return new ApiErrorResponse<>(errorMsg);
+        }
+    }
+
+    public class ApiSuccessResponse<T> extends ApiResponse<T>{ // api success
+        private T body;
+
+        ApiSuccessResponse(T body){
+            this.body = body;
+        }
+
+        public T getBody() {
+            return body;
+        }
+    }
+
+    public class ApiErrorResponse<T>extends ApiResponse<T>{ // api error
+
+        private String errorMessage;
+
+        ApiErrorResponse(String errorMessage){
+            this.errorMessage = errorMessage;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+
+    }
+
+    public class ApiEmptyResponse<T> extends ApiResponse<T>{ // api return empty
+
+    }
+
+}
--- MovieCentral/app/src/main/java/com/example/moviecentral/responses/MovieAPI.java
+++ MovieCentral/app/src/main/java/com/example/moviecentral/responses/MovieAPI.java
@@ -1,8 +1,9 @@
-package com.example.moviecentral.network;
+package com.example.moviecentral.responses;
+
+import androidx.lifecycle.LiveData;
 
 import com.example.moviecentral.models.GenreResponse;
 
-import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
@@ -12,7 +13,7 @@ public interface MovieAPI {
      * https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>&language=en-US
      */
     @GET("3/genre/movie/list")
-    Call<GenreResponse> getGenreList(
+    LiveData<ApiResponse<GenreResponse>> getGenreList(
             @Query("api_key") String apiKey,
             @Query("language") String language);
 
